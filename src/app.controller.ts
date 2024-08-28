90c1a4dd0c64b557470a9b930584466b5c9a09f4
Daniel Gualtero <esneidergonzales2020@gmail.com>
2024-08-28 13:19:48 -0500

    change for index rute

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,10 +4,10 @@ import { Controller, Get, Render } from '@nestjs/common';
 @Controller()
 export class AppController {
 
-  @Get()
+  @Get("/")
   @Render("index")
   root(){
-    return {saludo: "Hello Word"}
+    return {namePage: "Inicio - Logistica JB"}
   }
   
 }
