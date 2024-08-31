133c775eec741c6b44cba0e71e388d68fe0f074c
Daniel Gualtero <esneidergonzales2020@gmail.com>
2024-08-30 23:11:44 -0500

    add controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CotizacionController } from './cotizacion/cotizacion.controller';
+import { ServiceController } from './services/service.controller';
 
 
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController,ServiceController,CotizacionController],
   providers: [AppService],
 })
 export class AppModule {
