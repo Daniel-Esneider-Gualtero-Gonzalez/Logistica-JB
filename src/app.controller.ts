c1ff950e51bca9d25947a893ab0b6e5ea5d6523e
Daniel Gualtero <esneidergonzales2020@gmail.com>
2024-09-09 23:05:57 -0500

    gent and send opinions to page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
 
 
 @Controller()
 export class AppController {
+  constructor (private readonly appService :  AppService){}
 
   @Get("/")
   @Render("index")
   root(){
-    return {namePage: "Inicio - Logistica JB"}
+    return {namePage: "Inicio - Logistica JB", opinions: this.appService.getOpinions()}
   }
   
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,5 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+    getOpinions(){
+        let opinions =  []
+        for (let i = 0; i < 6; i++) {
+            const textOpinion = "int in ea non dolore. Enim minim magna anim id minim eu cillum sunt dolore aliquip. Amet elit laborum culpa irure incididunt adipisicing culpa amet officia exercitation. Eu non aute velit id velit Lorem elit anim pariatur" 
+            opinions.push({textOpinion : textOpinion})
+    
+        }
+        return opinions
+    }
  
 }
